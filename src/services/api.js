d058b6109a506e1d6c30bf54651ab0c2b1f04473
João Magalhães <joaomariomag@gmail.com>
2022-10-20 20:42:35 -0300

    Change start command

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const oldUrl = "http://localhost:3050/api"
-const herokuApiURl = 'https://projeto-integrador-5s-backend.herokuapp.com/'
+// const herokuApiURl = 'https://projeto-integrador-5s-backend.herokuapp.com/'
+const herokuApiURl = 'https://pi-5-semestre-backend.herokuapp.com/'
 
 const api = axios.create({
     baseURL: herokuApiURl,
