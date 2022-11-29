76ef555f7c854a83dfc5d865a07aedd7131196aa
João Magalhães <joaomariomag@gmail.com>
2022-11-29 09:19:58 -0300

    Change api url

--- src/services/api.js
+++ src/services/api.js
@@ -3,9 +3,10 @@ import axios from 'axios';
 const oldUrl = "http://localhost:3050/api"
 // const herokuApiURl = 'https://projeto-integrador-5s-backend.herokuapp.com/'
 const herokuApiURl = 'https://pi-5-semestre-backend.herokuapp.com/'
+const renderApiURl = 'https://pi-5-semestre-backend.onrender.com/'
 
 const api = axios.create({
-    baseURL: herokuApiURl,
+    baseURL: renderApiURl,
 });
 
 export default api;
